fb76a87f93de413443a6c64d6b157c0131df8739
jernacotillon <cotillonjerna@gmail.com>
2015-12-03 21:08:37 -0500

    Extract data of products (Expansys)

--- save22_productspiders/spiders/expansys.py
+++ save22_productspiders/spiders/expansys.py
@@ -1,8 +1,9 @@
 import scrapy
-from save22_productspiders.items import SampleItem
+from save22_productspiders.items import ExpansysItem
+from scrapy.spider import BaseSpider
 
-class ExpansysItem(scrapy.Spider):
-    name = "expan"
+class Expansys(scrapy.Spider):
+    name = "expans"
     allowed_domains = ["expansys.com.sg"]
     start_urls = [
         "http://www.expansys.com.sg/mobile-phones/",
@@ -24,20 +25,23 @@ class ExpansysItem(scrapy.Spider):
           yield request      
 
     def parse_dir_contents(self, response):
-        print "PUMASOK"
-        #items = list()
-
-        # for sel in response.xpath('//ul[@id=breadcrumbs"]'):
-        #   print sel
-            # item = SampleItem()
-            # item['url'] = response.url or None
-            # item['title'] = sel.xpath('@data-name').extract()
-            # item['description'] = sel.xpath('@data-desc').extract()
-            # item['img_urls'] = sel.xpath('@data-imgurl').extract()
-            # item['sel_qty'] = sel.xpath('@data-selqty').extract()
+        items = list()
+        
+        for sel in response.xpath('//ul[@id="breadcrumbs"]'):
+            item = ExpansysItem()
+            item['url'] = response.url or None
+            item['title'] = sel.xpath('//div[@id="title"]/h1/text()').extract()
+            item['description'] = sel.xpath('//div[@id="description"]/h2/text()').extract()
+            item['price'] = sel.xpath('//div[@id="prod_core"]/span/ul[@class="details"]/li[@class="price"]/p[@id="price"]/strong/span/text()').extract()
+            item['sku'] = sel.xpath('//div[@id="prod_core"]/ul/li[1]/span/text()').extract()
+            item['ean'] = sel.xpath('//div[@id="prod_core"]/ul/li[2]/span/text()').extract()
+            item['mpn'] = sel.xpath('//div[@id="prod_core"]/ul/li[3]/span/text()').extract()
+            item['brand'] = sel.xpath('//div[@id="prod_core"]/ul/li[4]/a/text()').extract()
+            item['currency'] = sel.xpath('//p[@id="price"]/meta/@content').extract()
+            item['img_urls'] = sel.xpath('//div[@id="prod_left"]/div[2]/a/img/@src').extract()
             # item['price'] = sel.xpath('@data-price').extract()
             # item['oldprice'] = sel.xpath('@data-oldprice').extract()
             # item['outofstock'] = sel.xpath('@data-outofstock').extract()
-            # items.append(item)
-            # yield item
-        
+            items.append(item)
+            yield item
+
